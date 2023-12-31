d3c5677f432209f388001cd4bf003297ee7e0b7e
daniel ung <daniel.ung@sjsu.edu>
2023-12-30 17:16:36 -0800

    added starter code for react and for fastAPI backend

---
+++ chatsjsu.py
@@ -0,0 +1,25 @@
+import os 
+from dotenv import load_dotenv,find_dotenv
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+from langchain.chat_models import ChatOpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import LLMChain
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.agents.agent_toolkits import create_python_agent
+from langchain.llms.openai import OpenAI 
+from langchain.document_loaders import PyPDFLoader
+import panel as pn
+import param
+
+
+#api keys 
+load_dotenv(find_dotenv())
+openAIKey = os.environ.get("OPEN_AI_KEY")
+huggingfaceKey = os.environ.get("HUGGINGFACEHUB_API_TOKEN")
+
+
+
